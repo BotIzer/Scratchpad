c48282518a28985d634c574e4c4e841342ba6c6e
BotIzer <izerboti@gmail.com>
2026-01-22 19:28:32 +0100

    Refactor, render/delete shape on button press

--- src/main.cpp
+++ src/main.cpp
@@ -11,20 +11,30 @@ void cursorMovedCallback(GLFWwindow* window, double x, double y){
     std::cout << "Cursor moved: x: " << x <<  std::endl << "y: " << y << std::endl; 
 }
 
-void processInput(GLFWwindow* window){
+void processInput(GLFWwindow* window, bool* render){
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     {
         glfwSetWindowShouldClose(window, true);
     }
+    if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS)
+    {
+        *render = true;
+        std::cout << "T Press";
+    }
+    if (glfwGetKey(window, GLFW_KEY_DELETE) == GLFW_PRESS)
+    {
+        *render = false;
+    }
+    
+    
     
 }
-
-void renderTriangle(unsigned int* VAO, unsigned int* VBO, unsigned int* shaderProgram){
+void createShader(unsigned int* shaderProgram){
 
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &basicShaderSource, NULL);
     glCompileShader(vertexShader);
-    // check for shader compile errors
+
     int success;
     char infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
@@ -33,23 +43,23 @@ void renderTriangle(unsigned int* VAO, unsigned int* VBO, unsigned int* shaderPr
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
-    // fragment shader
+
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
-    // check for shader compile errors
+
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
     if (!success)
     {
         glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
-    // link shaders
+
     *shaderProgram = glCreateProgram();
     glAttachShader(*shaderProgram, vertexShader);
     glAttachShader(*shaderProgram, fragmentShader);
     glLinkProgram(*shaderProgram);
-    // check for linking errors
+
     glGetProgramiv(*shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
         glGetProgramInfoLog(*shaderProgram, 512, NULL, infoLog);
@@ -57,27 +67,65 @@ void renderTriangle(unsigned int* VAO, unsigned int* VBO, unsigned int* shaderPr
     }
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
+}
 
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // left  
-         0.5f, -0.5f, 0.0f, // right 
-         0.0f,  0.5f, 0.0f  // top   
-    }; 
+const unsigned int selectShape(const Shape shape, float** vertices){
+    unsigned int siz = -1;
+    switch (shape)
+    {
+    case TRIANGLE:
+        *vertices = new float[9]
+        {
+           -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f,  0.5f, 0.0f 
+        };
+        siz = 9;
+        break;
+    
+    case RECTANGLE:
+        *vertices = new float[12]
+        {
+            0.5f,  0.5f, 0.0f, 
+            0.5f, -0.5f, 0.0f, 
+           -0.5f, -0.5f, 0.0f,
+           -0.5f,  0.5f, 0.0f
+        };
+        siz = 12;
+        break;
+    default:
+        *vertices = new float[9]
+        {
+           -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f,  0.5f, 0.0f 
+        };
+        siz = 9;
+        break;
+    }
+    return siz;
+}
+
+void renderShape(unsigned int* VAO, unsigned int* VBO){
+
+    float* vertices;
+    unsigned int siz = selectShape(TRIANGLE, &vertices);
+
+    if (siz == -1) std::cout << "Error getting size of shape" << std::endl;
+
     glGenVertexArrays(1, VAO);
     glGenBuffers(1,VBO);
     glBindVertexArray(*VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, *VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, siz * sizeof(float), vertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
 
-    glBindVertexArray(0); 
+    glBindVertexArray(0);
 }
 
 int main(){
@@ -87,7 +135,6 @@ int main(){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(viewport.width,viewport.height, "Scratchpad", NULL, NULL);
     if (window == NULL)
@@ -109,19 +156,23 @@ int main(){
 
 
     unsigned int VBO, VAO, shaderProgram;
-    renderTriangle(&VAO, &VBO, &shaderProgram);
+    createShader(&shaderProgram);
+    renderShape(&VAO, &VBO);
 
-
+    char in;
+    bool render = false;
     while (!glfwWindowShouldClose(window))
     {
-        processInput(window);
-
+        processInput(window, &render);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        if (render){
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+        }
+        
         
         glfwSwapBuffers(window);
         glfwPollEvents();
--- src/models.hpp
+++ src/models.hpp
@@ -5,3 +5,19 @@ typedef struct dimensions
     int width;
     int height;
 }dimensions;
+enum Shape{
+    TRIANGLE,
+    RECTANGLE
+};
+static const float verticesTri[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f 
+};
+static const float verticesRect[] = {
+     0.5f,  0.5f, 0.0f, 
+     0.5f, -0.5f, 0.0f, 
+    -0.5f, -0.5f, 0.0f,
+    -0.5f,  0.5f, 0.0f
+};
+
